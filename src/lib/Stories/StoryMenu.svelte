f5b44480ae6d283427a39cb61c65ae72beb93557
itlackey <itlackey@outlook.com>
2022-03-12 22:53:26 -0600

    spelling and story menu

--- src/lib/Stories/StoryMenu.svelte
+++ src/lib/Stories/StoryMenu.svelte
@@ -2,7 +2,6 @@
 	import Menu from '$lib/Menu/Menu.svelte';
 	import MenuItem from '$lib/Menu/MenuItem.svelte';
 	import { getStories } from './Store';
-
 	export let viewer;
 </script>
 
@@ -11,17 +10,12 @@
 		<span>Loading</span>
 	{:then stories}
 		{#each stories as story}
-			<MenuItem icon={story.icon} title={story.title} on:click={() => viewer.selectStory(story.slug)}
-				>{story.description}</MenuItem
-			>
+			<MenuItem
+				icon={story.icon}
+				title={story.title}
+				on:click={() => viewer.selectStory(story.slug)}
+				description={story.description}
+			/>
 		{/each}
 	{/await}
-	<!-- <MenuItem icon="bi bi-building" title="Welcome to Dimm City" on:click={() => viewer.selectStory('introduction')}>
-		An introduction to the world of Dimm City where the Sporos live
-	</MenuItem>
-	<MenuItem icon="bi bi-cpu-fill" title="Prologue - Scene 1" on:click={() => viewer.selectStory('prologue')}
-		>compiling...</MenuItem
-	>
-	<MenuItem icon="bi bi-terminal" title="Prologue - Scene 2">generating...</MenuItem>
-	<MenuItem icon="bi bi-safe" title="Locked">identity unknown</MenuItem> -->
 </Menu>
