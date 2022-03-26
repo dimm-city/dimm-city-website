21fea65fcb1bfc5d6f517244e4ee64f9421fa0e6
itlackey <itlackey@outlook.com>
2022-03-26 17:15:13 -0500

    dynamic config started

--- src/lib/config.staging.ts
+++ src/lib/config.staging.ts
@@ -1,8 +1,7 @@
-
 export const config = {
-	version: "0.2.0",
-	baseUrl:   "https://dimm.city",
-	graphUrl: 'https://data.dimm.city/graphql', //  "http://localhost:1337/graphql",
+	version: '0.2.0',
+	graphUrl: 'https://data.dimm.city/graphql',
+	baseUrl: 'azurewebsites.net',
 	releases: {
 		s1r1: {
 			name: 'Sporo Rabbits',
@@ -19,7 +18,7 @@ export const config = {
 					name: 'rinkeby',
 					chainId: 4,
 					address: '0x8Cfd5Fb8B806FDb5bf1Cd1949a2561Aa8b592Ef9', //"0x43Ea1a9d3945BB5A64e6C66506d55308525d8a87", // "0xbB2a603Ae60AeD5f538787bD55b1F37e544008B1", //"0x643908D555Bd3845F0D424eFd22e92C468AE360A",
-					metadataBaseUri: 'https://data.dimm.city/api/token-metadata/sporos/s1r1',
+					metadataBaseUri: 'https://data.dimm.city/api/token-metadata/sporos/s1r1'
 					//metadataBaseUri: 'http://localhost:1337/api/token-metadata/sporos/s1r1',
 				}
 			]
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,24 +1,12 @@
-export const config = {
-	version: '0.2.0',
-	graphUrl: 'http://localhost:1337/graphql',
-	releases: {
-		s1r1: {
-			name: 'Sporo Rabbits',
-			maxTokens: 10,
-			networks: [
-				{
-					name: 'mainnet',
-					chainId: 1,
-					address: '0xeF48DF1d388aa7222Da0B83d8b7CB265EF8d0B4a',
-					metadataBaseUri: 'http://localhost:1337/api/token-metadata/sporos/s1r1'
-				},
-				{
-					name: 'rinkeby',
-					chainId: 4,
-					address: '0x8Cfd5Fb8B806FDb5bf1Cd1949a2561Aa8b592Ef9', //"0x43Ea1a9d3945BB5A64e6C66506d55308525d8a87", // "0xbB2a603Ae60AeD5f538787bD55b1F37e544008B1", //"0x643908D555Bd3845F0D424eFd22e92C468AE360A",
-					metadataBaseUri: 'http://localhost:1337/api/token-metadata/sporos/s1r1'
-				}
-			]
-		}
-	}
-};
+import { config as prod } from './config.prod';
+import { config as dev } from './config.staging';
+import { browser } from '$app/env';
+
+//ToDo: add staging env
+export const config = isProduction() ? prod : dev;
+
+function isProduction() {
+	if (!browser && process && process.env && process.env.NODE_ENV === 'production') return true;
+	if (browser && window.location.href.includes(prod.baseUrl)) return true;
+	return false;
+}
