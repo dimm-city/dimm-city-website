4336d78190a34415aed011ba69e928775c7b1349
itlackey <itlackey@outlook.com>
2022-04-02 23:12:09 -0500

    better env detection

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,12 +1,14 @@
 import { config as prod } from './config.prod';
+import { config as staging } from './config.staging';
 import { config as dev } from './config.dev';
 import { browser } from '$app/env';
 
-//ToDo: add staging env
-export const config = isProduction() ? prod : dev;
+export const config = getCurrentConfig(); 
 
-function isProduction() {
-	if (!browser && process && process.env && process.env.NODE_ENV === 'production') return true;
-	if (browser && window.location.href.includes(prod.baseUrl)) return true;
-	return false;
+function getCurrentConfig() {
+	if (!browser && process && process.env && process.env.NODE_ENV === 'production') return prod;
+	if (browser && window.location.href.includes(prod.baseUrl)) return prod;
+	if (browser && window.location.href.includes(staging.baseUrl)) return staging;
+	if (browser && window.location.href.includes(dev.baseUrl)) return dev;
+	return dev;
 }
