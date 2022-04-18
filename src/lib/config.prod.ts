efaa756504bfa40024eeb945423a9b8dc6f547dd
itlackey <itlackey@outlook.com>
2022-04-18 17:01:59 -0500

    fixing config

--- src/lib/config.prod.ts
+++ src/lib/config.prod.ts
@@ -13,14 +13,14 @@ export const config = {
 					name: 'mainnet',
 					chainId: 1,
 					address: '0xeF48DF1d388aa7222Da0B83d8b7CB265EF8d0B4a',
-					metadataBaseUri: 'https://data.dimm.city/api/token-metadata/sporos/s1r1' // "/api/v1/sporos/s1r1",
+					metadataBaseUri: 'https://data.dimm.city/api/metadata/sporos/dcs1r1' // "/api/v1/sporos/s1r1",
 					//metadataBaseUri: 'http://localhost:1337/api/token-metadata/sporos/s1r1',
 				},
 				{
 					name: 'rinkeby',
 					chainId: 4,
 					address: '0x8Cfd5Fb8B806FDb5bf1Cd1949a2561Aa8b592Ef9', //"0x43Ea1a9d3945BB5A64e6C66506d55308525d8a87", // "0xbB2a603Ae60AeD5f538787bD55b1F37e544008B1", //"0x643908D555Bd3845F0D424eFd22e92C468AE360A",
-					metadataBaseUri: 'https://data.dimm.city/api/token-metadata/sporos/s1r1',
+					metadataBaseUri: 'https://data.dimm.city/api/metadata/sporos/dcs1r1',
 					//metadataBaseUri: 'http://localhost:1337/api/token-metadata/sporos/s1r1',
 				}
 			]
