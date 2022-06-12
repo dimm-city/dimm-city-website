70898fee6a23ecbb5445a79787f85ad723b68999
itlackey <itlackey@outlook.com>
2022-06-11 23:21:22 -0500

    moved specialties to local storage

--- src/lib/ShellStore.ts
+++ src/lib/ShellStore.ts
@@ -8,8 +8,8 @@ export const showMenu = writable(true);
 export const myCollection = writable(getSessionValue('collection') ?? []);
 myCollection.subscribe((value) => setSessionValue('collection', value));
 
-export const specialties = writable(getSessionValue('specialties') ?? []);
-specialties.subscribe((value) => setSessionValue('specialties', value));
+export const specialties = writable(getLocalValue('specialties') ?? []);
+specialties.subscribe((value) => setLocalValue('specialties', value, getExpiryTime()));
 
 export const characters = writable(getLocalValue('characters') ?? []);
 characters.subscribe((value) => setLocalValue('characters', value, getExpiryTime()));
