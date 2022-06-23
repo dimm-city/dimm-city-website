9c9c1da3a13c99db6e5e52dbfa5bd8610d553c9a
itlackey <itlackey@outlook.com>
2022-06-23 02:00:06 -0500

    basic journal entries working

---
+++ src/lib/JournalEntries/ViewJournalEntry.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import './journal-entries.css';
+	import Shell from '$lib/Shell.svelte';
+	import { showMenu } from '$lib/ShellStore';
+	import ContentPane from '$lib/Components/ContentPane.svelte';
+	import Menu from '$lib/Components/Menu/Menu.svelte';
+	import { getJournalEntry } from './getJournalEntryBySlug';
+	import Article from '$lib/Components/Article.svelte';
+	import Image from '$lib/Components/Image.svelte';
+	import JournalEntriesMenu from './JournalEntriesMenu.svelte';
+	export let slug;
+	$showMenu = false;
+	let model = null;
+
+	$: if (slug > '') {
+		model = null;
+		getJournalEntry(slug).then((s) => (model = s));
+	}
+</script>
+
+<Shell title="Journals">
+	<ContentPane padding={0} scrollable={true}>
+		{#if model}
+			<Article {model}>
+				<div slot="main-image">
+					{#if model.imageUrl > ''}
+						<Image
+							height=""
+							width=""
+							imageUrl={model.imageUrl}
+							title={model.name}
+							videoUrl={model.videoUrl}
+							aug="tl-clip tr-clip-y br-clip bl-clip-y"
+						/>
+					{/if}
+				</div>
+			</Article>
+		{/if}
+	</ContentPane>
+	<Menu slot="menu">
+		<JournalEntriesMenu bind:selectedItem={slug} />
+	</Menu>
+</Shell>
---
+++ src/routes/journal-entries/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Shell from '$lib/Shell.svelte';
+	import Menu from '$lib/Components/Menu/Menu.svelte';
+	import { showMenu } from '$lib/ShellStore';
+	import ContentPane from '$lib/Components/ContentPane.svelte';
+	import JournalEntriesMenu from '$lib/JournalEntries/JournalEntriesMenu.svelte';
+	$showMenu = true;
+</script>
+
+<Shell title="Journals" showMenuButton={false}>
+	<ContentPane>
+		<Menu />
+	</ContentPane>
+	<Menu slot="menu">
+		<JournalEntriesMenu />
+	</Menu>
+</Shell>
