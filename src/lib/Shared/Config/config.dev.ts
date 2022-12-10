190c9140b5eea0da5947693bdacf1fd9b6f8d029
itlackey <itlackey@outlook.com>
2022-12-10 09:54:33 -0600

    wtf

--- src/lib/Shared/Config/config.dev.ts
+++ src/lib/Shared/Config/config.dev.ts
@@ -2,6 +2,6 @@ export const config = {
 	version: '',
 	googleKey: '',
 	baseUrl:   "https://dimm.city",
-	graphUrl:  "http://localhost:1337/graphql",
+	graphUrl:  "http://data.dimm.city/graphql",
 	apiBaseUrl: 'https://data.dimm.city/api',
 };
