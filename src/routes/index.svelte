b7893f797729c1730de4272fa91a7d284f6f1ade
itlackey <itlackey@outlook.com>
2023-01-20 02:10:18 -0600

    added version to dashboard title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,10 @@
 	import Shell from '$lib/Shared/Components/Shell.svelte';
 	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
 	import Dashboard from '$lib/Dashboard/Dashboard.svelte';
+	import { config } from '$lib/Shared/config';
 </script>
 
-<Shell title="DCC Dashboard" fullscreen={true}>
+<Shell title={"DCC Dashboard " + config.version} fullscreen={true}>
 	<ContentPane padding={0}>
 		<Dashboard />
 	</ContentPane>
