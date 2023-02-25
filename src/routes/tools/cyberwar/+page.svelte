24a178ee86ee837a97072546e71486c1a5dbe934
itlackey <itlackey@outlook.com>
2023-02-25 12:57:51 -0600

    fixing cyberwar package

--- src/routes/tools/cyberwar/+page.svelte
+++ src/routes/tools/cyberwar/+page.svelte
@@ -1,7 +1,7 @@
 
 <script lang="ts">
     // import Game from "$lib/RootTerminal/Game.svelte";
-	import { Game } from "dc-cyberwar";
+	import { Game } from "@dimm-city/dc-cyberwar";
 	import Shell from '$lib/Shared/Components/Shell.svelte';
 	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
 	import SpecialtyMenu from '$lib/Specialties/SpecialtyMenu.svelte';
