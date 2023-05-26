eac8d694b01dccd8013862acef40e418afbc25e4
itlackey <itlackey@outlook.com>
2023-05-26 17:00:10 -0500

    dice and styling

--- src/routes/console/dreams/+page.svelte
+++ src/routes/console/dreams/+page.svelte
@@ -3,8 +3,10 @@
 	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
 	import LoggedInContainer from '$lib/Shared/Components/LoggedInContainer.svelte';
 	import { Game } from '@dimm-city/dc-solo-rpg';
-
+	import { onMount } from 'svelte';
 	let selectedPlayer = null;
+	let selectedDice = null;
+	let selectedGame = null;
 	const games = [
 		{ title: 'WAA Game Template', url: '/waa/Example.json' },
 		{ title: 'Future Lost', url: '/waa/future-lost.json' }
@@ -17,17 +19,29 @@
 			name: 'murmur'
 		}
 	];
+
+	let diceThemes = [
+		{ name: 'pink', color: '#ef1ebf' },
+		{ name: 'blue', color: '#3e6ab3' },
+		{ name: 'purple', color: '#c743ff' }
+	];
+
+	onMount(() => {
+		selectedDice = diceThemes.at(0);
+		selectedPlayer = players.at(1);
+		selectedGame = games.at(1);
+	});
 </script>
 
 <Shell title="Console">
 	<ContentPane padding={0}>
 		<!-- <div class="content-container"> -->
-			<!-- <LoggedInContainer>
+		<!-- <LoggedInContainer>
 				<div class="fade-in"> -->
-			<link rel="stylesheet" href="/assets/styles/WAAStyles.css" />
-			<Game {games} {players} selectedPlayer={players.at(1)} selectedGame={games.at(1)} />
+		<link rel="stylesheet" href="/assets/styles/WAAStyles.css" />
+		<Game {games} {players} {selectedPlayer} {selectedGame} {diceThemes} {selectedDice} />
 
-			<!-- </div>
+		<!-- </div>
 			</LoggedInContainer> -->
 		<!-- </div> -->
 	</ContentPane>
