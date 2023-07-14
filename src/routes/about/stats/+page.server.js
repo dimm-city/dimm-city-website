b6448e3aafab8d52210eb38a320a732f166bf799
itlackey <itlackey@outlook.com>
2023-07-14 15:48:37 -0500

    ts is evil

--- src/routes/about/stats/+page.server.js
+++ src/routes/about/stats/+page.server.js
@@ -1,16 +1,14 @@
 import { config } from '$lib/Shared/config';
-import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = async () => {
 	const response = await fetch(`${config.apiBaseUrl}/dimm-city/stats`);
 	if (response.ok) {
 		const json = await response.json();
 		json.env = config.env;
 		json.url = config.apiBaseUrl;
 		return json;
-	}
-	else {
+	} else {
 		console.error('Could not load stats', response);
 		return {};
 	}
-}) satisfies PageServerLoad;
+};
