b5a710af3bd904d46812bf96df18dac8d17d0e17
itlackey <itlackey@outlook.com>
2023-08-07 10:36:57 -0500

    added markdown loader

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,38 +5,8 @@
 	export let data;
 </script>
 
-<LandingShell videoUrl="https://files.dimm.city/assets/locations/the-dark-drab-edition.mp4">
+<LandingShell>
 	<article class="content-container">
-		<h1>
-			Welcome, fellow Dreamer, to the streets of Dimm City.
-		</h1>
-		<p>
-			Our cover artist, Scott Georges, has painted the backdrop of our dreamworld with hues of
-			sci-fi and cyberpunk, a world where your imagination holds the reins.
-		</p>
-
-		<p>
-			Dimm City, the crux of our tale, teeters between the tangible and the ethereal. It is a
-			metropolis sculpted from your deepest dreams and darkest nightmares, where the glimmering
-			skyscrapers meet the nebulous clouds of the dreamverse.
-		</p>
-
-		<p>
-			In this game, you're not just a bystander, but a luminary of lucid dreaming. As you weave
-			through the labyrinth of Dimm City, you become a puppeteer of your own fate, crafting scenes
-			that ripple through the dream fabric of this world.
-		</p>
-
-		<p>
-			You'll roll the dice, of course, but they're not the masters of your destiny. You are. Your
-			choices, your actions, and your dreams shape the course of your journey. You'll engage with
-			dream economies, manipulate distances, and navigate deadly scenes, all in a day's work.
-		</p>
-
-		<p>
-			Dimm City isn't just a game. It's a dreamscape that breathes and pulsates with your
-			imagination. Are you ready to wake up in this dream, Dreamer? Because in Dimm City, the limit
-			is not the sky, but your ability to dream.
-		</p>
+		{@html data.content}
 	</article>
 </LandingShell>
--- src/routes/about/overview/+page.svelte
+++ src/routes/about/overview/+page.svelte
@@ -1,50 +1,10 @@
 <script>
-	import LandingShell from '$lib/Shared/Shell/LandingShell.svelte';	
+	import LandingShell from '$lib/Shared/Shell/LandingShell.svelte';
+	export let data;
 </script>
 
 <LandingShell>
-    <article class="content-container">
-    <h1>System Overview</h1>
-	<p>
-		Welcome, Dreamers, to the overview of Dimm City RPG, where the essence of tabletop gaming melds
-		with the vibrancy of cyberpunk, the intrigue of sci-fi, and the magic of fantasy.
-	</p>
-
-	<p>
-		At its core, Dimm City RPG is a game of dreams and choices. It's not just about rolling dice,
-		but about how you, as a player, shape your own reality within the dream-laden metropolis of Dimm
-		City.
-	</p>
-
-	<p>
-		For TTRPG players, you'll revel in the unique blend of strategy and storytelling. Your choices
-		matter, with each action rippling through the dream-fabric of Dimm City. Intricate mechanics,
-		from dream economies to distance manipulation, create a gameplay depth that engages both your
-		mind and creativity.
-	</p>
-
-	<p>
-		If you're a cyberpunk enthusiast, Dimm City will feel like home. The cityscape is a blend of
-		high-tech marvels and low-life grit. Here, neon lights and shadowy alleys coexist, painting a
-		world that's as thrilling as it is unpredictable.
-	</p>
-
-	<p>
-		Sci-fi fans, you're in for a treat. The game dances on the blurred line between reality and
-		dreams, weaving a narrative that's both familiar and alien. The sci-fi elements add a layer of
-		mystery and exploration, pushing the boundaries of what you might consider 'real'.
-	</p>
-
-	<p>
-		Fantasy fans, fear not. The dream aspect infuses the game with an air of mystique and magic. In
-		Dimm City, the impossible becomes possible, and the mundane turns extraordinary. Your dreams are
-		your magic, bending and shaping the world around you.
-	</p>
-
-	<p>
-		In essence, Dimm City RPG is a playground for your imagination, a place where different genres
-		and themes converge to create something unique. It's not just a game—it's an experience. A
-		journey. Are you ready to dream, Dreamer?
-	</p>
-    </article>
+	<article class="content-container">
+		{@html data.content}
+	</article>
 </LandingShell>
