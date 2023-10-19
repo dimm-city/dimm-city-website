9c728a9864d1ddba5e45aaa17386b256ebf696a4
itlackey <itlackey@gmail.com>
2023-10-18 23:33:58 -0500

    stats formatting.

--- src/routes/console/about/+page.svelte
+++ src/routes/console/about/+page.svelte
@@ -6,37 +6,26 @@
 </script>
 
 <Shell title="Console Stats">
-	<ContentPane padding={1} scrollable={true}>
-		<h4 class="">Console Stats:</h4>
-		<ul class="unstyled-list">
-			<li>Console Environment: {config.env}</li>
-			<li>Console Version: {config.version}</li>
-			<li>Data Connection Version: {data.version}</li>
-			<li>Connection Status: Weak</li>
-			<li>Citizen Profiles recorded: {data.character}</li>
-			<li>Citizen Specialties recorded: {data.specialty}</li>
-			<li>Districts recorded: {data.location}</li>
-			<li>Journal entries recorded: {data["journal-entry"]}</li>		
-		</ul>
-	<!-- <li>License: <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-                CC-BY
-              </a>
-			</li> 
-			dependencies -->
-		<h4>technologies, projects, and contributions</h4>
-		<p>A true thank you to all of the people that poured hours of passion into these projects!</p>
-		<ul class="unstyled-list">
-			<li><a href="https://augmented-ui.com/">augmented-ui</a></li>
-			<li><a href="https://animate.style/">animate.css</a></li>
-			<li><a href="https://docs.ethers.io/v5/">ethers</a></li>
-			<li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
-			<li><a href="https://github.com/mattjennings/svelte-modals">svelte-modals</a></li>
-			<li><a href="https://github.com/EvanOxfeld/svelte-step-wizard">svelte-step-wizard</a></li>
-			<li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
-			<li><a href="https://eslint.org/">eslint</a></li>
-			<li><a href="https://prettier.io/">prettier</a></li>
-			<li><a href="https://strapi.io/">Strapi</a></li>
-			<li><a href="http://www.twinery.org/">Twine</a></li>
-		</ul>
+	<ContentPane padding={2} scrollable={true}>
+		<div>
+			<h3>Console Stats:</h3>
+			<ul class="unstyled-list">
+				<li>Environment: {config.env}</li>
+				<li>Version: {config.version}</li>
+				<li>Connection Version: {data.version}</li>
+				<li>Connection Status: Weak</li>
+				<li><a href="/citizens">Citizen Files</a>: {data.character}</li>
+				<li><a href="/specialties">Specialties</a>: {data.specialty}</li>
+				<li><a href="/locations">Locations</a>: {data.location}</li>
+				<li><a href="/journal-entries">Journal entries</a>: {data['journal-entry']}</li>
+			</ul>
+		</div>
+		
 	</ContentPane>
 </Shell>
+
+<style>
+	a {
+		display: inline;
+	}
+</style>
--- src/routes/console/about/dependencies/+page.svelte
+++ src/routes/console/about/dependencies/+page.svelte
@@ -1,38 +1,67 @@
 <script lang="ts">
 	import Shell from '$lib/Shared/Shell/Shell.svelte';
 	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
-
 </script>
 
 <Shell title="Dependencies and Integrations">
-	<ContentPane scrollable={true}>
-		<h4 class="">Dependencies and Integrations:</h4>
+	<ContentPane padding={2} scrollable={true}>
+		<div>
+			<!-- <li>License: <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+                CC-BY
+              </a>
+			</li> 
+			dependencies 
+		<h4>technologies, projects, and contributions</h4>
+		<p>A true thank you to all of the people that poured hours of passion into these projects!</p>
 		<ul class="unstyled-list">
-			<li><a rel="noreferrer" target="_blank" href="https://augmented-ui.com">augmented-ui</a></li>
-			<li><a rel="noreferrer" target="_blank" href="https://animate.style">animate.css</a></li>
-			<li>
-				<a rel="noreferrer" target="_blank" href="https://github.com/ethers-io/ethers.js/">ethers</a
-				>
-			</li>
-			<li><a rel="noreferrer" target="_blank" href="https://kit.svelte.dev">SvelteKit</a></li>
-			<li>
-				<a rel="noreferrer" target="_blank" href="https://github.com/mattjennings/svelte-modals"
-					>svelte-modals</a
-				>
-			</li>
-			<li>
-				<a rel="noreferrer" target="_blank" href="https://npmjs.org/svelte-step-wizard">svelte-step-wizard</a>
-			</li>
-			<li><a rel="noreferrer" target="_blank" href="https://www.typescriptlang.org">TypeScript</a></li>
-			<li><a rel="noreferrer" target="_blank" href="https://eslint.org">eslint</a></li>
-			<li><a rel="noreferrer" target="_blank" href="https://prettier.io">prettier</a></li>
-			<li><a rel="noreferrer" target="_blank" href="https://strapi.io">Strapi</a></li>
-			<li><a rel="noreferrer" target="_blank" href="https://twinery.org">Twine</a></li>
-			<li>
-				<a rel="noreferrer" target="_blank" href="https://github.com/tmedwards/sugarcube-2"
-					>sugarcube-2</a
-				>
-			</li>
-		</ul>
+			<li><a href="https://augmented-ui.com/">augmented-ui</a></li>
+			<li><a href="https://animate.style/">animate.css</a></li>
+			<li><a href="https://docs.ethers.io/v5/">ethers</a></li>
+			<li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
+			<li><a href="https://github.com/mattjennings/svelte-modals">svelte-modals</a></li>
+			<li><a href="https://github.com/EvanOxfeld/svelte-step-wizard">svelte-step-wizard</a></li>
+			<li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+			<li><a href="https://eslint.org/">eslint</a></li>
+			<li><a href="https://prettier.io/">prettier</a></li>
+			<li><a href="https://strapi.io/">Strapi</a></li>
+			<li><a href="http://www.twinery.org/">Twine</a></li>
+		</ul>-->
+
+			<h3 class="">Dependencies and Integrations:</h3>
+			<ul class="unstyled-list">
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://augmented-ui.com">augmented-ui</a>
+				</li>
+				<li><a rel="noreferrer" target="_blank" href="https://animate.style">animate.css</a></li>
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://github.com/ethers-io/ethers.js/"
+						>ethers</a
+					>
+				</li>
+				<li><a rel="noreferrer" target="_blank" href="https://kit.svelte.dev">SvelteKit</a></li>
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://github.com/mattjennings/svelte-modals"
+						>svelte-modals</a
+					>
+				</li>
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://npmjs.org/svelte-step-wizard"
+						>svelte-step-wizard</a
+					>
+				</li>
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://www.typescriptlang.org">TypeScript</a>
+				</li>
+				<li><a rel="noreferrer" target="_blank" href="https://eslint.org">eslint</a></li>
+				<li><a rel="noreferrer" target="_blank" href="https://prettier.io">prettier</a></li>
+				<li><a rel="noreferrer" target="_blank" href="https://strapi.io">Strapi</a></li>
+				<li><a rel="noreferrer" target="_blank" href="https://twinery.org">Twine</a></li>
+				<li>
+					<a rel="noreferrer" target="_blank" href="https://github.com/tmedwards/sugarcube-2"
+						>sugarcube-2</a
+					>
+				</li>
+			</ul>
+		</div>
 	</ContentPane>
 </Shell>
