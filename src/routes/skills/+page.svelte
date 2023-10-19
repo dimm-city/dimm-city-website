56efef2654941fd4d59681079fe49b9bdd86f289
itlackey <itlackey@gmail.com>
2023-10-19 02:35:01 -0500

    WIP: skill tree spike

---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Shell from '$lib/Shared/Shell/Shell.svelte';
+	import SearchPage from '$lib/Shared/Components/SearchPage.svelte';
+	import MenuItem from '$lib/Shared/Components/Menu/MenuItem.svelte';
+	import DefaultItemResult from '$lib/Shared/Components/DefaultItemResult.svelte';
+
+	export let data;
+
+	let query = {};
+	const endpoint = '/dimm-city/skill-trees';
+</script>
+
+<Shell title="Skill Trees">
+	<SearchPage {endpoint} initialData={data}>
+		<svelte:fragment slot="result" let:result>
+			<MenuItem url={`/skills/${result.attributes.slug}`}>
+				<DefaultItemResult
+					icon="bi-tools"
+					subtitle={result.attributes.type}
+					item={result.attributes}
+				/>
+			</MenuItem>
+		</svelte:fragment>
+	</SearchPage>
+</Shell>
---
+++ src/routes/skills/[slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import SkillTree from '$lib/Abilities/SkillTree.svelte';
+	import ItemView from '$lib/Items/ItemView.svelte';
+	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
+	import Shell from '$lib/Shared/Shell/Shell.svelte';
+    /**
+	 * @type {DC.SkillTree}
+	 */
+	export let data;
+</script>
+
+<Shell title={data?.attributes?.name} titleUrl="/skills">
+	<ContentPane padding={2} scrollable={true}>
+		<SkillTree {data} />		
+	</ContentPane>
+</Shell>
