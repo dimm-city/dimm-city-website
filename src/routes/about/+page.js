aa40ff2e4fa3cd2447642f46b38d51751cf5f5d4
itlackey <itlackey@gmail.com>
2023-10-31 19:43:27 -0500

    about wtf

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -8,8 +8,11 @@ import { loadEntityPageFromStrapi } from '$lib/Shared/SvelteStrapi';
  * @param {string} page.params.slug - The slug parameter.
  * @return {Promise<any>} A promise that resolves to the loaded entity page.
  */
-export const load = async (/** @type {{ params: { slug: any; }; }} */ page) =>
-	await loadEntityPageFromStrapi(page, 'dimm-city/pages');
+export const load = async (/** @type {{ params: { slug: any; }; }} */ page) => {
+	page.params.slug = 'about';
+	const result = await loadEntityPageFromStrapi(page, 'dimm-city/pages');
+	return result;
+};
 
 export const prerender = true;
 export const csr = true;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,19 +6,20 @@
 	export let data;
 
 	onMount(() => {
+		console.log(data);
 		const styleElement = document.getElementById('page-style');
-		if (styleElement) styleElement.innerHTML = data.attributes.styles;
+		if (styleElement) styleElement.innerHTML = data.attributes?.styles;
 	});
 </script>
 
 <svelte:head>
-	<meta name="description" content={data.attributes.description} />
-	<meta name="keywords" content={data.attributes.tags} />
+	<meta name="description" content={data.attributes?.description} />
+	<meta name="keywords" content={data.attributes?.tags} />
 </svelte:head>
-<LandingShell title={data.attributes.title}>
+<LandingShell title={data?.attributes?.title}>
 	<style id="page-style"></style>
-	<article class="content-container" style={data.attributes.styles}>
-		<h1>{data.attributes.title}</h1>
-		{@html marked.parse(data.attributes.content)}
+	<article class="content-container" style={data.attributes?.styles}>
+		<h1>{data.attributes?.title}</h1>
+		{@html marked.parse(data.attributes?.content ?? '')}
 	</article>
 </LandingShell>
