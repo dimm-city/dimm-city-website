4b5d3488d636ac9aa0ced08e20887e0869925288
itlackey <itlackey@gmail.com>
2023-11-03 00:07:33 -0500

    Added new Svelte page for dynamic routes.

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import LandingShell from '$lib/Shared/Shell/LandingShell.svelte';
+	import { marked } from 'marked';
+	import { onMount } from 'svelte';
+    
+	export let data;
+
+	onMount(() => {
+		const styleElement = document.getElementById('page-style');
+		if (styleElement) styleElement.innerHTML = data.attributes?.styles;
+	});
+</script>
+
+<svelte:head>
+	<meta name="description" content={data.attributes?.description} />
+	<meta name="keywords" content={data.attributes?.tags} />
+</svelte:head>
+<LandingShell title={data?.attributes?.title}>
+	<style id="page-style"></style>
+	<article class="content-container" style={data.attributes?.styles}>
+		<h1>{data.attributes?.title}</h1>
+		{@html marked.parse(data.attributes?.content ?? '')}
+	</article>
+</LandingShell>
