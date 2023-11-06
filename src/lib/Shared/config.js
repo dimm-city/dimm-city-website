7389b4dea0f9bbd07ce318c45b992c7596b26e01
itlackey <itlackey@gmail.com>
2023-11-06 00:43:49 -0600

    update version

--- src/lib/Shared/config.js
+++ src/lib/Shared/config.js
@@ -11,7 +11,7 @@ function getCurrentConfig() {
 	/** @type {DC.Config} */
 	let output = {
 		...dev,
-		version: '0.9.0',
+		version: '0.10.0',
 		defaultSummaryQuery: {
 			sort: ['name:asc'],
 			populate: 'mainImage',
