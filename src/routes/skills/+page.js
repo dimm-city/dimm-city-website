a468565ef539a1df3102a11d59ea305ab9bd6579
itlackey <itlackey@gmail.com>
2023-11-09 00:26:10 -0600

    moving skills page

--- src/routes/skills/+page.js
+++
@@ -1,9 +0,0 @@
-import { loadSearchPageFromStrapi } from '$lib/Shared/SvelteStrapi';
-export const load = async (page) => {
-	return await loadSearchPageFromStrapi(
-		page,
-		'dimm-city/skill-trees',
-		['slug', 'name', 'shortDescription'],
-		['mainImage']
-	);
-};
