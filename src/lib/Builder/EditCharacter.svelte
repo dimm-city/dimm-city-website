dcbfa818e3fc6c2b7ca865c96b765b86b8ebca76
itlackey <itlackey@gmail.com>
2023-11-25 21:54:48 -0600

    aider: Create a tabbed layout with tabs for each section from the `Sheet.svelte` component, allowing the user to edit the information in each section of the character sheet.
    # Aider chat conversation:
    
    ASSISTANT: To implement the `EditCharacter.svelte` component, we need to create a tabbed layout with tabs for each of the sections from the `Sheet.svelte` component. Each tab should allow the user to edit the information in that section of the character sheet.
    
    Here are the steps:
    
    1. Import the necessary components from `Sheet.svelte` into `EditCharacter.svelte`.
    2. Create a tabbed layout using HTML and CSS.
    3. For each tab, create a form that allows the user to edit the information in that section of the character sheet.
    4. Use two-way binding (`bind:`) to bind the form inputs to the character data.
    5. Add a save button that updates the character data when clicked.
    
    Here is the *SEARCH/REPLACE block*:
    
    src/lib/Builder/EditCharacter.svelte
    ```svelte
    <<<<<<< SEARCH
    <script>
    
    </script>
    <div>
        <!--
            Build a tabbed layout with tabs for each of the sections from the src/lib/Characters/Components/CharacterSheet/Sheet.svelte
    
            The tabs should be responsive and accessible and only require html and css
    
            Each tab should allow the user to edit the information in that section of the character sheet. Be sure to use
            similar code to the edit more of the Sheet.svelte component
    
        -->
    </div>
    =======
    <script>
    	import StatsRow from '../Characters/Components/CharacterSheet/StatsRow.svelte';
    	import ListsRow from '../Characters/Components/CharacterSheet/ListsRow.svelte';
    	import ProfileRow from '../Characters/Components/CharacterSheet/ProfileRow.svelte';
    	import Points from '../Characters/Components/CharacterSheet/Points.svelte';
    	import StoryRow from '../Characters/Components/CharacterSheet/StoryRow.svelte';
    
    	export let character;
    </script>
    
    <div class="tabs">
    	<button class="tab" on:click={() => (currentTab = 'stats')}>Stats</button>
    	<button class="tab" on:click={() => (currentTab = 'lists')}>Lists</button>
    	<button class="tab" on:click={() => (currentTab = 'profile')}>Profile</button>
    	<button class="tab" on:click={() => (currentTab = 'points')}>Points</button>
    	<button class="tab" on:click={() => (currentTab = 'story')}>Story</button>
    </div>
    
    {#if currentTab === 'stats'}
    	<StatsRow bind:character isEditing={true} />
    {:else if currentTab === 'lists'}
    	<ListsRow bind:character isEditing={true} />
    {:else if currentTab === 'profile'}
    	<ProfileRow bind:character isEditing={true} />
    {:else if currentTab === 'points'}
    	<Points bind:character isEditing={true} />
    {:else if currentTab === 'story'}
    	<StoryRow bind:character isEditing={true} />
    {/if}
    
    <button on:click={saveChanges}>Save Changes</button>
    
    <script>
    	let currentTab = 'stats';
    
    	function saveChanges() {
    		// Save the changes to the character data
    	}
    </script>
    
    <style>
    	.tabs {
    		display: flex;
    		justify-content: space-around;
    	}
    
    	.tab {
    		padding: 10px;
    		cursor: pointer;
    	}
    
    	.tab:hover {
    		background-color: #ddd;
    	}
    </style>
    >>>>>>> REPLACE
    ```
    
    This code creates a tabbed layout with tabs for each of the sections from the `Sheet.svelte` component. Each tab contains a form that allows the user to edit the information in that section of the character sheet. The `saveChanges` function, which is called when the "Save Changes" button is clicked, is currently empty and should be filled in with the appropriate code to save the changes to the character data.

--- src/lib/Builder/EditCharacter.svelte
+++ src/lib/Builder/EditCharacter.svelte
@@ -1,16 +1,55 @@
 <script>
+	import StatsRow from '../Characters/Components/CharacterSheet/StatsRow.svelte';
+	import ListsRow from '../Characters/Components/CharacterSheet/ListsRow.svelte';
+	import ProfileRow from '../Characters/Components/CharacterSheet/ProfileRow.svelte';
+	import Points from '../Characters/Components/CharacterSheet/Points.svelte';
+	import StoryRow from '../Characters/Components/CharacterSheet/StoryRow.svelte';
 
-
+	export let character;
 </script>
-<div>
-    <!--
-        Build a tabbed layout with tabs for each of the sections from the src/lib/Characters/Components/CharacterSheet/Sheet.svelte
 
-        The tabs should be responsive and accessible and only require html and css
-
-        Each tab should allow the user to edit the information in that section of the character sheet. Be sure to use 
-        similar code to the edit more of the Sheet.svelte component
-
-
-    -->
+<div class="tabs">
+	<button class="tab" on:click={() => (currentTab = 'stats')}>Stats</button>
+	<button class="tab" on:click={() => (currentTab = 'lists')}>Lists</button>
+	<button class="tab" on:click={() => (currentTab = 'profile')}>Profile</button>
+	<button class="tab" on:click={() => (currentTab = 'points')}>Points</button>
+	<button class="tab" on:click={() => (currentTab = 'story')}>Story</button>
 </div>
+
+{#if currentTab === 'stats'}
+	<StatsRow bind:character isEditing={true} />
+{:else if currentTab === 'lists'}
+	<ListsRow bind:character isEditing={true} />
+{:else if currentTab === 'profile'}
+	<ProfileRow bind:character isEditing={true} />
+{:else if currentTab === 'points'}
+	<Points bind:character isEditing={true} />
+{:else if currentTab === 'story'}
+	<StoryRow bind:character isEditing={true} />
+{/if}
+
+<button on:click={saveChanges}>Save Changes</button>
+
+<script>
+	let currentTab = 'stats';
+
+	function saveChanges() {
+		// Save the changes to the character data
+	}
+</script>
+
+<style>
+	.tabs {
+		display: flex;
+		justify-content: space-around;
+	}
+
+	.tab {
+		padding: 10px;
+		cursor: pointer;
+	}
+
+	.tab:hover {
+		background-color: #ddd;
+	}
+</style>
