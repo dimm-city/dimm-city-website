8c8d34487a806ce22f189caf9392d051bb827cc2
itlackey <itlackey@gmail.com>
2023-11-25 21:54:46 -0600

    Added EditCharacter component to the src/lib/Builder directory.

---
+++ src/lib/Builder/EditCharacter.svelte
@@ -0,0 +1,16 @@
+<script>
+
+
+</script>
+<div>
+    <!--
+        Build a tabbed layout with tabs for each of the sections from the src/lib/Characters/Components/CharacterSheet/Sheet.svelte
+
+        The tabs should be responsive and accessible and only require html and css
+
+        Each tab should allow the user to edit the information in that section of the character sheet. Be sure to use 
+        similar code to the edit more of the Sheet.svelte component
+
+
+    -->
+</div>
