31b4ddbb140b312f56c8173337d92e634231d57c
itlackey <itlackey@gmail.com>
2023-12-01 00:20:39 -0600

    Added AppearanceEditor component.

---
+++ src/lib/Builder/AppearanceEditor.svelte
@@ -0,0 +1,9 @@
+<!--
+Implement a new svelte component based on the StatsRow.svelte component. However this component should remove the cybernetics section
+and make the image more prominent in the row.
+
+It should also allow editing Age, Height, and Weight. The code for this can be found the in the src/lib/Builder/ProfileEditor.svelteHTML.
+Also include the ability to toggle between metric and imperial units for the editing of the  Height, and Weight. Toggling the unit should update
+the displayed value for the given stat. However the actual value should always be stored in metric units.
+
+-->
