85e89ce731dc470b3c1e2c5d7e4300dcf6adda83
itlackey <itlackey@gmail.com>
2023-12-03 23:56:37 -0600

    remove notification context from create page

--- src/routes/console/archive/characters/create/+page.svelte
+++ src/routes/console/archive/characters/create/+page.svelte
@@ -2,17 +2,14 @@
 	import CreateCharacter from '$lib/Characters/CreateCharacter.svelte';
 	import ContentPane from '$lib/Shared/Components/ContentPane.svelte';
 	import Shell from '$lib/Shared/Shell/Shell.svelte';
-	import Notifications from 'svelte-notifications';
-	import  Alert from "$lib/Shared/Shell/Alert.svelte";
 </script>
 
-<Notifications item={Alert} zIndex={300}>
-	<Shell title="Create a Character" fullscreen={true}>
-		<ContentPane>
-			<CreateCharacter />
-		</ContentPane>
-		<div slot="left-button">
-			<!-- <a
+<Shell title="Create a Character" fullscreen={true}>
+	<ContentPane>
+		<CreateCharacter />
+	</ContentPane>
+	<div slot="left-button">
+		<!-- <a
 		
 		class="aug-button"
 		data-augmented-ui="all-hex"
@@ -21,6 +18,5 @@
 	>
 		<i class="bi bi-x-octagon" /></a
 	> -->
-		</div>
-	</Shell>
-</Notifications>
+	</div>
+</Shell>
